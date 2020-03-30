af78a1daf86fdf4fcc159d9c8d2b96131239e909
Voggastur <v3x0r_x014@hotmail.com>
2020-03-30 18:05:31 +0000

    search user function in github.html

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,9 +15,8 @@ function userInformationHTML(user) {
         </div>`;
 }
 
-
-
 function fetchGitHubInformation(event) {
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -29,17 +28,21 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-    $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
         function(response) {
             var userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse) {
+        },
+        function(errorResponse) {
             if (errorResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: $(errorResponse.responseJSON.message)</h2>`);
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
